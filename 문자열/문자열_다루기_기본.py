78d995c2d4420053b323414d12d535b30bbe4dc5
swdevsw98 <102571918+swdevsw98@users.noreply.github.com>
2023-07-15 23:04:27 +0900

    문자열 다루기 기본

---
+++ 문자열/문자열_다루기_기본.py
@@ -0,0 +1,12 @@
+def solution(s):
+    answer = True
+    
+    if len(s) == 4 or len(s) == 6:
+        if s.isdigit() :
+            pass
+        else :
+            answer = False
+    else :
+        answer = False
+    
+    return answer
